b9bd1449aa1abd95d0156653d78a202606bcb9d0
Egor <azazaz.com534@gmail.com>
2023-07-04 21:30:44 +0300

    initial commit

---
+++ src/main/java/com/exam/kursova9_2kurs/Kursova92kursApplication.java
@@ -0,0 +1,13 @@
+package com.exam.kursova9_2kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursova92kursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursova92kursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exam/kursova9_2kurs/Kursova92kursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.kursova9_2kurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursova92kursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
